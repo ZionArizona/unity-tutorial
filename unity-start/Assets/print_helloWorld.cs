1367e3609dced830c68dc7cdab33ef262b0b8316
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2023-01-14 01:07:00 +0900

    C# basic programming language

---
+++ unity-start/Assets/print_helloWorld.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class print_helloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("hello world.");    
+    }
+
+    
+}
